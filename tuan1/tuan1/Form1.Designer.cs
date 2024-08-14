5e3ec32457f1bd705cdb0d553944b84959e15793
mikee thanh <mikeethanh04@gmail.com>
2024-08-14 23:14:26 +0700

    init

---
+++ tuan1/tuan1/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace tuan1
+{
+    partial class giaiphuongtrinh
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txta = new System.Windows.Forms.TextBox();
+            this.txtc = new System.Windows.Forms.TextBox();
+            this.txtb = new System.Windows.Forms.TextBox();
+            this.btngpt = new System.Windows.Forms.Button();
+            this.btnlamlai = new System.Windows.Forms.Button();
+            this.txtkq = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label1.Location = new System.Drawing.Point(28, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "a:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label2.Location = new System.Drawing.Point(28, 127);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "b:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label3.Location = new System.Drawing.Point(28, 191);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "c:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // txta
+            // 
+            this.txta.Location = new System.Drawing.Point(84, 64);
+            this.txta.Multiline = true;
+            this.txta.Name = "txta";
+            this.txta.Size = new System.Drawing.Size(81, 34);
+            this.txta.TabIndex = 3;
+            // 
+            // txtc
+            // 
+            this.txtc.Location = new System.Drawing.Point(84, 191);
+            this.txtc.Multiline = true;
+            this.txtc.Name = "txtc";
+            this.txtc.Size = new System.Drawing.Size(81, 38);
+            this.txtc.TabIndex = 4;
+            // 
+            // txtb
+            // 
+            this.txtb.Location = new System.Drawing.Point(84, 117);
+            this.txtb.Multiline = true;
+            this.txtb.Name = "txtb";
+            this.txtb.Size = new System.Drawing.Size(81, 35);
+            this.txtb.TabIndex = 5;
+            // 
+            // btngpt
+            // 
+            this.btngpt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btngpt.Location = new System.Drawing.Point(576, 102);
+            this.btngpt.Name = "btngpt";
+            this.btngpt.Size = new System.Drawing.Size(127, 46);
+            this.btngpt.TabIndex = 6;
+            this.btngpt.Text = "giai pt";
+            this.btngpt.UseVisualStyleBackColor = true;
+            this.btngpt.Click += new System.EventHandler(this.btngpt_Click);
+            // 
+            // btnlamlai
+            // 
+            this.btnlamlai.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnlamlai.Location = new System.Drawing.Point(576, 179);
+            this.btnlamlai.Name = "btnlamlai";
+            this.btnlamlai.Size = new System.Drawing.Size(127, 42);
+            this.btnlamlai.TabIndex = 7;
+            this.btnlamlai.Text = "lam lai";
+            this.btnlamlai.UseVisualStyleBackColor = true;
+            this.btnlamlai.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtkq
+            // 
+            this.txtkq.Enabled = false;
+            this.txtkq.Location = new System.Drawing.Point(222, 64);
+            this.txtkq.Multiline = true;
+            this.txtkq.Name = "txtkq";
+            this.txtkq.Size = new System.Drawing.Size(324, 165);
+            this.txtkq.TabIndex = 8;
+            // 
+            // giaiphuongtrinh
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 280);
+            this.Controls.Add(this.txtkq);
+            this.Controls.Add(this.btnlamlai);
+            this.Controls.Add(this.btngpt);
+            this.Controls.Add(this.txtb);
+            this.Controls.Add(this.txtc);
+            this.Controls.Add(this.txta);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "giaiphuongtrinh";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txta;
+        private System.Windows.Forms.TextBox txtc;
+        private System.Windows.Forms.TextBox txtb;
+        private System.Windows.Forms.Button btngpt;
+        private System.Windows.Forms.Button btnlamlai;
+        private System.Windows.Forms.TextBox txtkq;
+    }
+}
+
---
+++ tuan1/tuan1/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tuan1
+{
+    public partial class giaiphuongtrinh : Form
+    {
+        public giaiphuongtrinh()
+        {
+            InitializeComponent();
+        }
+        ptb2 pt;
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btngpt_Click(object sender, EventArgs e)
+        {
+            double a, b, c;
+            a =double.Parse(txta.Text);
+            b = Convert.ToDouble(txtb.Text);
+            bool k;
+            k = double.TryParse(txtc.Text, out c);
+            if (k)
+            {
+                pt = new ptb2(a, b, c);
+                var kq = pt.giaipt();
+                txtkq.Text = kq;
+                txtkq.ForeColor = Color.Red;
+            }
+            
+        }
+    }
+}
